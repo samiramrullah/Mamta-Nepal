79ad01c1ccb93c03a7c93f3b51fbec9764335198
samiramrullah <samiramrullah@gmail.com>
2022-11-16 22:44:36 +0530

    footer link set

--- frontend/src/common/Navbar.tsx
+++ frontend/src/common/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className='bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200'>
