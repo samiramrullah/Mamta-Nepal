073bc74090f41b0587f5d1ceb831b0aca17c1c37
samiramrullah <samiramrullah@gmail.com>
2022-11-18 00:39:45 +0530

    package-lock json removed

--- frontend/src/common/Login/LoginCard.tsx
+++ frontend/src/common/Login/LoginCard.tsx
@@ -16,26 +16,26 @@ const LoginCard = () => {
                                 <img className="absolute inset-0 z-0 object-cover w-full h-full ml-auto test"
                                     src={sideimage} alt="Mamta Nepal" />
                                 <div className="top-0 z-10 max-w-xl mx-auto mb-12 text-center ">
-                                    <h2 className="mb-4 text-4xl font-bold text-gray-100 dark:text-gray-300 ">
+                                    <h2 className="mb-4 text-4xl font-bold text-gray-100  ">
                                         Welcome to our community and join with us</h2>
                                     <div className="max-w-lg mx-auto mb-6">
-                                        <p className="pt-6 font-medium text-gray-300 dark:text-gray-300">
+                                        <p className="pt-6 font-medium text-gray-300 ">
                                             lorem ipsum dor amet sidcuscd andih wkoidus iusoyions hejitywa qopasation dummy text
                                             ipsum
                                         </p>
                                     </div>
                                     <Link to={`/login/signup`}
-                                        className="inline-block px-6 py-2 font-medium bg-red-500 text-gray-50 dark:text-gray-300">
+                                        className="inline-block px-6 py-2 font-medium bg-red-500 text-gray-50 ">
                                         Join now</Link>
                                 </div>
                             </div>
                             {login === "login" ? (
-                                <div className="w-full py-6   lg:py-7 lg:w-1/2 dark:bg-gray-900">
+                                <div className="w-full py-6   lg:py-7 lg:w-1/2 ">
                                     <div className="max-w-md mx-auto">
                                         <div className="px-4 my-7 ">
                                             <div className="mb-7">
                                                 <span
-                                                    className="flex items-center justify-center w-20 h-20 mx-auto text-gray-900 bg-red-500 rounded-lg dark:bg-green-600 ">
+                                                    className="flex items-center justify-center w-20 h-20 mx-auto text-gray-900 bg-red-500 rounded-lg ">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
                                                         fill="currentColor" className="text-gray-200 bi bi-person-circle"
                                                         viewBox="0 0 16 16">
@@ -46,26 +46,26 @@ const LoginCard = () => {
                                                     </svg>
                                                 </span>
                                             </div>
-                                            <h2 className="mb-4 text-xl font-bold lg:mb-8 lg:text-3xl dark:text-gray-400">
+                                            <h2 className="mb-4 text-xl font-bold lg:mb-8 lg:text-3xl ">
                                                 Login our account</h2>
                                             <form action="" className="p-0 m-0">
                                                 <div>
                                                     <label
-                                                        className="text-lg font-medium text-gray-700 dark:text-gray-400">Email:</label>
+                                                        className="text-lg font-medium ">Email:</label>
                                                     <input type="email"
-                                                        className="w-full px-4 py-3 mt-3 bg-gray-200 rounded-lg dark:text-gray-400 dark:bg-gray-800 "
+                                                        className="w-full px-4 py-3 mt-3 bg-gray-200 rounded-lg   "
                                                         name="" placeholder="Enter your email" />
                                                 </div>
                                                 <div className="mt-5">
                                                     <div>
                                                         <label
-                                                            className="text-lg font-medium text-gray-700 dark:text-gray-400 ">Password:</label>
+                                                            className="text-lg font-medium  ">Password:</label>
                                                         <div className="relative flex items-center mt-2">
                                                             <input type="password"
-                                                                className="w-full px-4 py-3 bg-gray-200 rounded-lg dark:text-gray-400 dark:bg-gray-800 "
+                                                                className="w-full px-4 py-3 bg-gray-200 rounded-lg   "
                                                                 name="" placeholder="Enter password" />
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                                className="absolute right-0 mr-3 dark:text-gray-50  bi bi-eye-slash"
+                                                                className="absolute right-0 mr-3   bi bi-eye-slash"
                                                                 fill="currentColor" viewBox="0 0 16 16">
                                                                 <path
                                                                     d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
@@ -79,16 +79,16 @@ const LoginCard = () => {
                                                 </div>
                                                 <div className="mt-5 text-right">
                                                     <Link to={'/'}
-                                                        className="text-sm font-semibold text-blue-700 dark:text-blue-300 dark:hover:text-blue-500">
+                                                        className="text-sm font-semibold text-blue-700  ">
                                                         forgot password?</Link>
                                                 </div>
                                                 <button
                                                     className="w-full px-4 py-3 mt-5 font-semibold text-gray-200 bg-red-500 rounded-lg  hover:bg-red-600"
                                                     type="submit">LOGIN</button>
                                                 <div
-                                                    className="flex flex-wrap items-center mt-3 text-sm text-gray-700 lg:text-base lg:mt-5 dark:text-gray-400">
+                                                    className="flex flex-wrap items-center mt-3 text-sm lg:text-base lg:mt-5 ">
                                                     Need an account?
-                                                    <Link to={`/login/${false}`} className="ml-2 text-base font-semibold text-blue-700 dark:text-blue-300 dark:hover:text-blue-500">
+                                                    <Link to={`/login/${false}`} className="ml-2 text-base font-semibold ">
                                                         Create an account</Link>
                                                 </div>
                                             </form>
@@ -97,34 +97,34 @@ const LoginCard = () => {
                                 </div>
 
                             ) : (
-                                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                                <div className="w-full  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                                             Create and account
                                         </h1>
                                         <form className="space-y-4 md:space-y-6" action="#">
                                             <div>
-                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                                             </div>
                                             <div>
-                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                             </div>
                                             <div>
-                                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                                <label className="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label>
+                                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                             </div>
                                             <div className="flex items-start">
                                                 <div className="flex items-center h-5">
-                                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300   dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
                                                 </div>
                                                 <div className="ml-3 text-sm">
-                                                    <label className="font-light text-gray-500 dark:text-gray-300">I accept the <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to={'/'}>Terms and Conditions</Link></label>
+                                                    <label className="font-light text-gray-500 ">I accept the <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to={'/'}>Terms and Conditions</Link></label>
                                                 </div>
                                             </div>
                                             <button type="submit" className="w-full text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                            <p className="text-sm font-light text-gray-500 ">
                                                 Already have an account? <Link to={`/login/${true}`} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
                                             </p>
                                         </form>
