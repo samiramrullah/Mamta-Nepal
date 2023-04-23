b80c68c96edcf8bb6d9f47eba071a58dfa5d9341
samiramrullah <samiramrullah@gmail.com>
2023-04-23 20:10:48 +0530

    register route added

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const userSchema=mongoose.Schema({
     _id:mongoose.Types.ObjectId,
     name:{type:String,required:true},
-    email:{type:String, required:true, unique},
+    email:{type:String, required:true, unique:true},
     password:{type:String,required:true}
 })
 
--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -1,10 +1,29 @@
-const express=require('express')
-const router=express.Router();
+const express = require('express')
+const router = express.Router();
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const mongoose=require('mongoose')
+const userSchema = require('../models/user')
 
-router.post('/register',(req,res,next)=>{
-    res.status(200).json({
-        message:'Post Route'
-    })
+router.post('/register', async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) return res.status(400).json({ message: 'Invalid data' })
+        if (!validator.isEmail(email)) return res.status(400).json({ message: "Invalid Email" })
+
+        //if email already exist
+        const user = await userSchema.findOne({ email })
+        if (user) return res.status(400).json({ message: "Email Already in use" })
+
+        //save to database
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const newUser = new userSchema({ _id: new mongoose.Types.ObjectId, name, email, password: hashedPassword })
+        newUser.save()
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (error) {
+        next(error);
+    }
 })
 
-module.exports=router;
+module.exports = router
