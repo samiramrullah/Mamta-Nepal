db8aba0f9a01bbe251353f6cbe1cc95a53425fbb
samiramrullah <samiramrullah@gmail.com>
2023-04-23 20:33:05 +0530

    log in route added

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
-const mongoose=require('mongoose')
+const jwt=require('jsonwebtoken')
+const mongoose = require('mongoose')
 const userSchema = require('../models/user')
 
 router.post('/register', async (req, res, next) => {
@@ -23,6 +24,23 @@ router.post('/register', async (req, res, next) => {
         res.status(201).json({ message: 'User registered successfully.' });
     } catch (error) {
         next(error);
+    }
+})
+
+router.post('/login', async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) return res.status(400).json({ message: 'Invalid Data' });
+        if (!validator.isEmail(email)) return res.status(400).json({ message: 'Invalid email' });
+
+        const user = await userSchema.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid email or password.' });
+        }
+        const token = jwt.sign({ userId: user.id, email: user.email, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Login successful.', token });
+    } catch (error) {
+        return res.status(401).json({ message: 'failed to login' });
     }
 })
 
