201c8f122980a10b0fd403131fec734fa27700f8
samiramrullah <samiramrullah@gmail.com>
2023-04-23 16:51:16 +0530

    Basic backend added

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app=require('./app')
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+const server=http.createServer(app)
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
