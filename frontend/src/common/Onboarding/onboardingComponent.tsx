4ee5931d775e0f0c163fa875d74ec32efb390ebe
Bishwash <bishwash.pokhrel1@gmail.com>
2023-05-02 00:08:12 +0530

    Added different component in onboarding stepper

--- frontend/src/common/Onboarding/onboardingComponent.tsx
+++ frontend/src/common/Onboarding/onboardingComponent.tsx
@@ -15,40 +15,39 @@ enum PregnancyStatus {
     Mother="Mother"
 }
 
+const ImageCard = () => {
+    return (
+    <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+    <div className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" 
+    style={{backgroundImage:"https://as1.ftcdn.net/v2/jpg/03/55/12/10/1000_F_355121089_YGnAXWWQgqVOK67otY6mA9Zjrkfstc9H.jpg"}}></div>
+
+    <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+        <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
+
+        <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span className="font-bold text-gray-800 dark:text-gray-200">$129</span>
+            <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+        </div>
+    </div>
+</div>
+    )
+}
+
+
+
 const PregnancyCard = ({setPregnancyStatus,pregnancyStatus}:PregnancyCardType)=>{
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
 		<div role="button" onClick={()=>{
             setPregnancyStatus(PregnancyStatus.Pregnant);
-            }} className={`w-48 p-6 rounded-2xl ${pregnancyStatus=== PregnancyStatus.Pregnant?"border-red-600":"border-black"}`}>
-			<div className="flex items-center cursor-pointer">
-				<span className="flex items-center justify-center w-6 h-6 rounded-full bg-pink-100">
-					<svg className="w-4 h-4 stroke-current text-pink-600"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-					</svg>
-				</span>
-				<span className="ml-2 text-sm font-medium text-gray-500">Followers</span>
-			</div>
-			<span className="block text-4xl font-semibold mt-4">1,320</span>
-			<div className="flex text-xs mt-3 font-medium">
-				<span className="text-green-500">+8%</span>
-				<span className="ml-1 text-gray-500">last 14 days</span>
-			</div>
+            }} 
+            className={`w-1/2 p-3 mx-4 sm:mx-20 my-4 sm:my-8 rounded-2xl border-2 m ${pregnancyStatus === PregnancyStatus.Pregnant?"border-red-600":"border-white"}`}>
+                <ImageCard/>
+
 		</div>
-		<div role="button" onClick={()=>{setPregnancyStatus(PregnancyStatus.Mother)}} className={`w-48 bg-white shadow-2xl p-6 rounded-2xl ${pregnancyStatus=== PregnancyStatus.Mother?"border-red-600":"border-black"}`}>
-			<div className="flex items-center cursor-pointer">
-				<span className="flex items-center justify-center w-6 h-6 rounded-full bg-red-100">
-					<svg className="w-4 h-4 stroke-current text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-					</svg>
-				</span>
-				<span className="ml-2 text-sm font-medium text-gray-500">Likes</span>
-			</div>
-			<span className="block text-4xl font-semibold mt-4">3,814</span>
-			<div className="flex text-xs mt-3 font-medium">
-				<span className="text-green-500">+12%</span>
-				<span className="ml-1 text-gray-500">last 14 days</span>
-			</div>
+		<div role="button" onClick={()=>{setPregnancyStatus(PregnancyStatus.Mother)}} 
+        className={`w-1/2 p-3 mx-4 sm:mx-20 my-4 sm:my-8 rounded-2xl border-2 ${pregnancyStatus=== PregnancyStatus.Mother?"border-red-600":"border-white"}`}>
+                <ImageCard/>
 		</div>
 	</div>
     )
@@ -81,7 +80,7 @@ export const OnboardingComponent = () => {
     const [pregnancyStatus,setPregnancyStatus] = useState("Pregnant");
 
     return (
-        <Modal open={true}>
+        <Modal open={true} className={"sm:w-2/3"}>
             <>
             <Paper>
                 <form>
